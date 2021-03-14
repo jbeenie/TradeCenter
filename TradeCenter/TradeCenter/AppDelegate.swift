8c2b4c743348efca43466ab75ab4db27a00ba822
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2021-03-14 10:59:09 -0700

    Create Xcode project

---
+++ TradeCenter/TradeCenter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TradeCenter
+//
+//  Created by Jeremie Benhamron on 2021-03-14.
+//  Copyright © 2021 Beenie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TradeCenter/TradeCenter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TradeCenter
+//
+//  Created by Jeremie Benhamron on 2021-03-14.
+//  Copyright © 2021 Beenie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
