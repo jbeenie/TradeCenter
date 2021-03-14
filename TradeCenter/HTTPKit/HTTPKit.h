1855f4d497cf65832651ed51e12dc1a7442151b9
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2021-03-14 12:11:14 -0700

    Add HTTPKit framework target, Create TradeCenter, HTTPKit folders to separate logic

---
+++ TradeCenter/HTTPKit/HTTPKit.h
@@ -0,0 +1,19 @@
+//
+//  HTTPKit.h
+//  HTTPKit
+//
+//  Created by Jeremie Benhamron on 2021-03-14.
+//  Copyright © 2021 Beenie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTPKit.
+FOUNDATION_EXPORT double HTTPKitVersionNumber;
+
+//! Project version string for HTTPKit.
+FOUNDATION_EXPORT const unsigned char HTTPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPKit/PublicHeader.h>
+
+
